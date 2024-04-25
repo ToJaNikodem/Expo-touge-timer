f0212d0c4a3f63091a19ac5a6af1d2a74e5df5ac
ToJaNikodem <nikodembuczkowski05@gmail.com>
2024-04-25 10:54:04 +0100

    123

--- App.js
+++ App.js
@@ -1,55 +1,40 @@
-import React, { useState, useEffect } from 'react';
-import { Platform, Text, View, StyleSheet } from 'react-native';
-import MapView from 'react-native-maps';
+// import React, { useState, useEffect } from 'react';
+// import { Platform, Text, View, StyleSheet } from 'react-native';
+// import { SafeAreaView } from 'react-native-safe-area-context';
+// import MapView from 'react-native-maps';
 
-import * as Location from 'expo-location';
+// import * as Location from 'expo-location';
 
-export default function App() {
-  const [location, setLocation] = useState(null);
-  const [errorMsg, setErrorMsg] = useState(null);
+// export default function App() {
+//   const [location, setLocation] = useState(null);
+//   const [errorMsg, setErrorMsg] = useState(null);
 
-  useEffect(() => {
-    (async () => {
+//   useEffect(() => {
+//     (async () => {
       
-      let { status } = await Location.requestForegroundPermissionsAsync();
-      if (status !== 'granted') {
-        setErrorMsg('Permission to access location was denied');
-        return;
-      }
+//       let { status } = await Location.requestForegroundPermissionsAsync();
+//       if (status !== 'granted') {
+//         setErrorMsg('Permission to access location was denied');
+//         return;
+//       }
 
-      let location = await Location.getCurrentPositionAsync({});
-      setLocation(location);
-    })();
-  }, []);
+//       let location = await Location.getCurrentPositionAsync({});
+//       setLocation(location);
+//     })();
+//   }, []);
 
-  let text = 'Waiting..';
-  if (errorMsg) {
-    text = errorMsg;
-  } else if (location) {
-    text = JSON.stringify(location);
-  }
+//   let text = 'Waiting..';
+//   if (errorMsg) {
+//     text = errorMsg;
+//   } else if (location) {
+//     //text = JSON.stringify(location);
+//     text = "lol";
+//   }
 
-  return (
-    <View style={styles.container}>
-      <Text style={styles.paragraph}>{text}</Text>
-      <MapView style={styles.map} />
-    </View>
-  );
-}
+//   return (
+//     <SafeAreaView style={styles.container}>
+//       <Text style={styles.paragraph}>{text}</Text>
+//     </SafeAreaView>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  paragraph: {
-    fontSize: 18,
-    textAlign: 'center',
-  },
-  map: {
-    width: '100%',
-    height: '100%',
-  },
-});
